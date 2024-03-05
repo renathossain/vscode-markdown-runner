2742d9f0cbc3f85027da78581d6cfd7cb6fe17ec
renathossain <renathossain@gmail.com>
2024-03-04 23:07:26 -0500

    get active terminal or if it does not exist create a new one

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('markdown.run.python', () => {
-			const terminal = vscode.window.createTerminal();
+			const terminal = vscode.window.activeTerminal || vscode.window.createTerminal();
 			terminal.show();
 		})
 	);
