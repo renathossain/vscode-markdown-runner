2a157d80aed24704372405bc0d3b76ffd0084677
renathossain <renathossain@gmail.com>
2024-03-04 23:16:13 -0500

    abstracted away the runCommandInTerminal function

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
 import * as vscode from 'vscode';
 
+function runCommandInTerminal(command: string) {
+    const terminal = vscode.window.activeTerminal || vscode.window.createTerminal();
+    terminal.show();
+    terminal.sendText(command);
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('markdown.run.python', () => {
-			const terminal = vscode.window.activeTerminal || vscode.window.createTerminal();
-			terminal.show();
+			runCommandInTerminal('python test.py');
 		})
 	);
 }
