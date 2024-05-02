0864d15e7543fe3ae2d124652a4b03e9f86cf22a
renathossain <renathossain@gmail.com>
2024-05-01 21:03:56 -0400

    Completed Refactor of Code

--- src/extension.ts
+++ src/extension.ts
@@ -14,144 +14,54 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// *********ARCHITECTURE*********
+// 
+
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
-import * as cp from 'child_process';
-import { ButtonCodeLensProvider } from './codeLens';
+import { ButtonCodeLensProvider, provideCommand } from './codeLens';
 import { getLanguageConfigurations } from './compilerConfig';
+import { cleanTempFiles, runCommandsInTerminal, executeJavaBlock, executeCodeBlock } from './codeRunner';
 
-// Stores the paths of the temporary files created for running code
-const tempFilePaths: string[] = [];
-
-// Stores the language configurations
+// Read and store the language configurations as a global variable
 export const languageConfigurations = getLanguageConfigurations();
 
-// Run the code line by line the terminal 
-export function runCommandsInTerminal(code: string) {
-    const terminal = vscode.window.activeTerminal || vscode.window.createTerminal();
-    terminal.show();
-    terminal.sendText(code);
-}
-
-// If successful compilation, it allows it execute the file
-// Otherwise, it throws an error message
-function compileHandler(command: string, callback: (success: boolean) => void): void {
-    cp.exec(command, (error, stdout, stderr) => {
-        if (error) {
-            // timeout is necessary because of interference with codelens
-            setTimeout(() => {
-                vscode.window.showErrorMessage(stderr, { modal: true });
-            }, 100);
-            callback(false);
-        } else {
-            callback(true);
-        }
-    });
-}
-
-function executeJavaBlock(code: string, extension: string, compiler: string) {
-    vscode.window.showInputBox({
-        prompt: 'Enter the name of the Java file (without extension). ' +
-            'Note: The Java standard requires the filename to be the ' +
-            'same as the name of the main class.',
-        placeHolder: 'MyJavaFile'
-    }).then((javaCompiledName) => {
-        if (javaCompiledName) {
-            const javaCompiledPath = path.join(os.tmpdir(), javaCompiledName);
-            const javaSourcePath = `${javaCompiledPath}.${extension}`;
-
-            fs.writeFileSync(javaSourcePath, code);
-            tempFilePaths.push(javaSourcePath);
-
-            compileHandler(`${compiler} ${javaSourcePath}`, (success) => {
-                if (success) {
-                    runCommandsInTerminal(`java -cp ${os.tmpdir()} ${javaCompiledName}`);
-                    tempFilePaths.push(`${javaCompiledPath}.class`);
-                }
-            });
-        }
-    });
-}
-
-// Save code to a temporary file and execute it
-// For compiled languages, a child process is created for compilation additionally
-function executeCodeBlock(code: string, extension: string, compiler: string) {
-    const compiledName = `temp_${Date.now()}`;
-    const compiledPath = path.join(os.tmpdir(), compiledName);
-    const sourcePath = `${compiledPath}.${extension}`;
-
-    fs.writeFileSync(sourcePath, code);
-    tempFilePaths.push(sourcePath);
-
-    if (extension === 'c' || extension === 'cpp' || extension === 'rs') {
-        compileHandler(`${compiler} -o ${compiledPath} ${sourcePath}`, (success) => {
-            if (success) {
-                runCommandsInTerminal(compiledPath);
-                tempFilePaths.push(compiledPath);
-            }
-        });
-    } else {
-        runCommandsInTerminal(`${compiler} ${sourcePath}`);
-    }
-}
-
-// Assigns what each command should do
-function registerCommand(context: vscode.ExtensionContext, commandId: string,
-    extension?: string, compiler?: string) {
+// Create the commands and assign what they do
+function registerCommand(context: vscode.ExtensionContext, language: string) {
     context.subscriptions.push(
-        vscode.commands.registerCommand(commandId, async (code: string) => {
-            if (commandId === 'markdown.copy') {
+        vscode.commands.registerCommand(provideCommand(language), async (code: string) => {
+            if (language === 'copy') {
                 await vscode.env.clipboard.writeText(code);
                 vscode.window.showInformationMessage('Code copied to clipboard.');
-            } else if (commandId === 'markdown.run.terminal') {
+            } else if (language === '') {
                 await runCommandsInTerminal(code);
-            } else if (extension && compiler) {
-                if (commandId === 'markdown.run.java') {
-                    await executeJavaBlock(code, extension, compiler);
-                } else {
-                    await executeCodeBlock(code, extension, compiler);
-                }
+            } else if (language === 'java') {
+                await executeJavaBlock(code);
+            } else {
+                await executeCodeBlock(language, code);
             }
         })
     );
 }
 
 // Main function that runs when the extension is activated
-// - Initializes and runs the code lens buttons
-// - Handles requests for running and copying Markdown Code Blocks
 export function activate(context: vscode.ExtensionContext) {
+    // Initializes the code lens buttons
     context.subscriptions.push(
         vscode.languages.registerCodeLensProvider({ language: 'markdown', scheme: 'file' },
             new ButtonCodeLensProvider()
         )
     );
 
-    // Read the langauge configuration from settings file
-    // and register the commands and buttons
-    const languageConfigurations = getLanguageConfigurations();
-    if (languageConfigurations) {
-        for (const [key, value] of Object.entries(languageConfigurations)) {
-            const commandId = `markdown.run.${key}`;
-            registerCommand(context, commandId, value.extension, value.compiler);
-        }
-    } else {
-        vscode.window.showErrorMessage('Language configurations not found.');
+    // Register the commands, which the code lens buttons will activate
+    for (const language of Object.keys(languageConfigurations)) {
+        registerCommand(context, language);
     }
-
-    registerCommand(context, 'markdown.run.terminal');
-    registerCommand(context, 'markdown.copy');
+    registerCommand(context, '');
+    registerCommand(context, 'copy');
 }
 
 // Deletes the temporary files that were generated during the extension's usage
 // when the extension is deactivated or VS Code is closed
 export function deactivate() {
-    tempFilePaths.forEach(filePath => {
-        try {
-            fs.unlinkSync(filePath);
-        } catch (error) {
-            vscode.window.showErrorMessage(`Error deleting file ${filePath}:`);
-        }
-    });
+    cleanTempFiles();
 }
